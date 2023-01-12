082f8b23d7ade56b5fda00d03a651f65ab8cf109
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2023-01-12 12:10:44 -0300

    feat: PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-
 @Module({
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: 'localhost',
+  //     port: 3306,
+  //     username: 'root',
+  //     password: 'root',
+  //     database: 'mini_hackthon',
+  //     entities: [],
+  //     synchronize: true,
+  //   })
+  // ],
+
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'mini_hackthon',
-      entities: [],
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       synchronize: true,
-    })
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [],
   providers: [],
